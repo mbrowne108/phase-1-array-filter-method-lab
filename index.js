fa56e794b8159b89aaf3417b19fc527720bda3a4
Matt <mbrowne108@gmail.com>
2021-08-19 12:26:32 -0400

    commit tree fc687fb02a2461a44a333e0c8144ab268c7ac006

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(array, name) {
+    let newArray = []
+    array.filter(function(driver){
+      if (driver.toLowerCase() === name.toLowerCase()){newArray.push(driver)}
+    })
+    return newArray;
+}
+
+function fuzzyMatch(array, letters) {
+    let newArray = []
+    array.filter(function(driver){
+      if (driver.substring(0,2) === letters){newArray.push(driver)}
+    })
+    return newArray;
+}
+
+function matchName(array, name) {
+    let newArray = []
+    array.filter(function(driver){
+      if (driver.name === name){newArray.push(driver)}
+    })
+    return newArray;
+}
